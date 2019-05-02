12a180011b9ddd765a5b8b4ad79c8b73adf7e49c
Michael Peterson <WebDev@ctoms.ca>
2019-05-02 15:54:10 -0600

    set up initial js files

---
+++ js/extractVariant.js
@@ -0,0 +1,4 @@
+//use this class to handle all functions related to extracting the variants
+function ExtractVariant(){
+
+}
---
+++ js/index.js
@@ -0,0 +1,13 @@
+//use this class to order the sequence of functions from the extract variant class
+//this class can contain all event listeners
+function App(){
+	//let converter = new ReorderCSV("inputFile","drop_zone","testData","deleteData","drop_zone_item_codes","removeItemCodes","reorderData","adjustItemCodes");
+	//let filterItemCodes = new FilterItemCodes(dropAreaItemCodesID);
+}
+
+App.prototype.initApp = function() {
+	console.log("start app");
+};
+
+let app = new App();
+window.onload = app.initApp();
